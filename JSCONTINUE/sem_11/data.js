f22e60adaebea07ea58cb6498fef886b9c6f697b
GricaevSergey <gricaev_sergey@mail.ru>
2024-08-08 16:09:36 +0500

    сделал задание из 11 семинара!

---
+++ JSCONTINUE/sem_11/data.js
@@ -0,0 +1,22 @@
+const dataProducts = {
+	"cards":[
+		{
+			"id": 1,
+			"discription": "MANGO  PEOPLE  T-SHIRT",
+			"url": "/JSlearnFirest/JSCONTINUE/sem_11/img/MANGO.jpg",
+			"price": 300,
+			"color": "red",
+			"size": "XI",
+			"quantity": 2
+		},
+		{
+			"id": 2,
+			"discription": "ELLERY X M'O CAPSULE",
+			"url": "/JSlearnFirest/JSCONTINUE/sem_11/img/ELLERY.jpg",
+			"price": 52,
+			"color": "red",
+			"size": "XI",
+			"quantity": 1
+		},
+	]
+}
---
+++ JSCONTINUE/sem_11/script.js
@@ -0,0 +1,28 @@
+'use strict';
+const cardsEl = document.querySelector('.cards');
+console.log(dataProducts.cards[0].discription);
+
+dataProducts.cards.forEach(element =>{
+	cardsEl.insertAdjacentHTML('beforeend', `
+		<div class="cardWrap">
+        <div class="imgWrap">
+          <img class="imgCard" src="${element.url}" alt="${element.id}" />
+        </div>
+        <div class="discriptionWrap">
+          <div class="titleItem">${element.discription}</div>
+          <div class="priceItem">Price:<span class="price">${element.price}</span></div>
+          <div class="colorItem">Color: <span class="color">${element.color}</span></div>
+          <div class="sizeItem">Size: <size class="size">${element.size}</size></div>
+          <div class="quantityItem">
+            Quantity: <span class="quantity">${element.quantity}</span>
+          </div>
+        </div>
+        <div class="close">
+          <img
+            src="/JSlearnFirest/JSCONTINUE/sem_11/img/close.svg"
+            alt="close"
+          />
+        </div>
+      </div>
+		`)
+})
